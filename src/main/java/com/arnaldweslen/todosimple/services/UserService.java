8cf4c4ad22dc1b76370a3d1998bc75aee8abf49d
ArnaldWeslen05 <arnaldclash@gmail.com>
2024-08-17 05:03:25 -0300

    RefactorUserService

--- src/main/java/com/arnaldweslen/todosimple/services/UserService.java
+++ src/main/java/com/arnaldweslen/todosimple/services/UserService.java
@@ -7,43 +7,36 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.arnaldweslen.todosimple.models.User;
-import com.arnaldweslen.todosimple.repositories.TaskRepository;
 import com.arnaldweslen.todosimple.repositories.UserRepository;
-
-
 
 @Service
 public class UserService {
-    
+
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private TaskRepository taskRepository;
-
-    public User findByid(long id){
+    public User findByid(long id) {
         Optional<User> user = this.userRepository.findById(id);
         return user.orElseThrow(() -> new RuntimeException(
-            "Usuario não encontrado! Id: " + id + ", Tipo: "+ User.class.getName()
+                "Usuario não encontrado! Id: " + id + ", Tipo: " + User.class.getName()
         ));
     }
 
-   @Transactional
+    @Transactional
     public User create(User obj) {
-         obj.setId(0);
+        obj.setId(0);
         obj = this.userRepository.save(obj);
-        this.taskRepository.saveAll(obj.getTasks());
         return obj;
     }
 
     @Transactional
-    public User update(User obj){
+    public User update(User obj) {
         User newObj = findByid(obj.getId());
         newObj.setPassword(obj.getPassword());
         return this.userRepository.save(newObj);
     }
 
-    public void delete(long id){
+    public void delete(long id) {
         findByid(id);
         try {
             this.userRepository.deleteById(id);
